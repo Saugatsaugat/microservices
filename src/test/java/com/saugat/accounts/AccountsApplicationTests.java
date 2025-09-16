b283a25d99fb3c56e59c8c1b110628ae887bd6db
Saugat Thapa <saugatthapa2073@gmail.com>
2025-09-16 11:15:31 -0500

    Accounts microservices project initialization

---
+++ src/test/java/com/saugat/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saugat.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
