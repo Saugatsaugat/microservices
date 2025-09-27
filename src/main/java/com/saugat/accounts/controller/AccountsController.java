a2822e99b265134fbee196a4db97f6ed0196fcf1
Saugat Thapa <saugatthapa2073@gmail.com>
2025-09-27 09:52:54 -0500

    Added code to perform input validations inside AccountsController. Updated Global exception handler to handle exception generated by invalid operations

--- src/main/java/com/saugat/accounts/controller/AccountsController.java
+++ src/main/java/com/saugat/accounts/controller/AccountsController.java
@@ -4,21 +4,25 @@ import com.saugat.accounts.constants.AccountsConstants;
 import com.saugat.accounts.dto.CustomerDto;
 import com.saugat.accounts.dto.ResponseDto;
 import com.saugat.accounts.service.IAccountsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody @Valid CustomerDto customerDto){
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -26,7 +30,9 @@ public class AccountsController {
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber){
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                                                               @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
+                                                               String mobileNumber){
         CustomerDto customerDto = iAccountsService.fetchAccountDetails(mobileNumber);
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -34,7 +40,7 @@ public class AccountsController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody @Valid CustomerDto customerDto){
         boolean isUpdated = iAccountsService.updateAccountsDetails(customerDto);
         if(isUpdated){
             return ResponseEntity
@@ -48,7 +54,9 @@ public class AccountsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam String mobileNumber){
+    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam
+                                                         @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
+                                                         String mobileNumber){
         boolean isDeleted = iAccountsService.deleteAccountDetails(mobileNumber);
         if(isDeleted){
             return ResponseEntity
--- src/main/java/com/saugat/accounts/dto/CustomerDto.java
+++ src/main/java/com/saugat/accounts/dto/CustomerDto.java
@@ -1,14 +1,24 @@
 package com.saugat.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message = "Name can not be null or empty")
+    @Size(min = 5, max = 30, message = "The name length must be between 5 and 30")
     private String name;
 
+    @NotEmpty(message = "Email can not be null or empty")
+    @Email(message = "Email should be valid")
     private String email;
 
+    @NotEmpty(message = "Mobile number can not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
 
     private AccountsDto accountsDto;
