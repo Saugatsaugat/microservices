7c1a74898a02381a98a10d5134fe75dddd6dd014
Saugat Thapa <saugatthapa2073@gmail.com>
2025-09-27 18:16:34 -0500

    Cards microservice added

---
+++ cards/src/main/java/com/saugat/cards/dto/ErrorResponseDto.java
@@ -0,0 +1,36 @@
+package com.saugat.cards.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Schema(
+        name = "Error Response",
+        description = "Schema to hold error related responses"
+)
+@Data @AllArgsConstructor
+public class ErrorResponseDto {
+
+    @Schema(
+            description = "API path invoked by client"
+    )
+    private String apiPath;
+
+    @Schema(
+            description = "Error code representing the error happened"
+    )
+    private HttpStatus errorCode;
+
+    @Schema(
+            description = "Error message representing the error happened"
+    )
+    private String errorMessage;
+
+    @Schema(
+            description = "Error time representing the error happened"
+    )
+    private LocalDateTime errorTime;
+}
---
+++ cards/src/main/java/com/saugat/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.saugat.cards.repository;
+
+import com.saugat.cards.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
