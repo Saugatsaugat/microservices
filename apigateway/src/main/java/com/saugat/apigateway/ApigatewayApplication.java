42dc9af7d1038f3115ac4d697c89b981bd088580
Saugat Thapa <saugatthapa2073@gmail.com>
2025-10-19 23:16:03 -0500

    Added custom response header in the route

--- apigateway/src/main/java/com/saugat/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/saugat/apigateway/ApigatewayApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class ApigatewayApplication {
 
@@ -16,15 +18,18 @@ public class ApigatewayApplication {
         return routeLocatorBuilder.routes()
                 .route(p -> p
                         .path("/eazybank/accounts/**")
-                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/eazybank/loans/**")
-                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://LOANS"))
                 .route(p -> p
                         .path("/eazybank/cards/**")
-                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS")).build();
     }
 
