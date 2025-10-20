5b12a0c1a19e7aec74d92f15757bc84401847d84
Saugat Thapa <saugatthapa2073@gmail.com>
2025-10-19 22:48:33 -0500

    Added configurations related to custom routing using gateway server

--- apigateway/src/main/java/com/saugat/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/saugat/apigateway/ApigatewayApplication.java
@@ -2,6 +2,8 @@ package com.saugat.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class ApigatewayApplication {
@@ -9,5 +11,21 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
+
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/eazybank/accounts/**")
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/eazybank/loans/**")
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/eazybank/cards/**")
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS")).build();
+    }
 
 }
