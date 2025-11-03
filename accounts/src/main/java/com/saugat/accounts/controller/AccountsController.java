2b63b8900c320989a069314fcfdc93c0ae25adb7
Saugat Thapa <saugatthapa2073@gmail.com>
2025-11-03 09:35:17 -0600

    Feature | added rate limiter configuration in accounts microservice for getJavaVersion method

--- accounts/src/main/java/com/saugat/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/saugat/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.saugat.accounts.dto.CustomerDto;
 import com.saugat.accounts.dto.ErrorResponseDto;
 import com.saugat.accounts.dto.ResponseDto;
 import com.saugat.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -182,11 +183,18 @@ public class AccountsController {
                     )
             )}
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallBack")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.toString());
+    }
+
+    public ResponseEntity<String> getJavaVersionFallBack(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
